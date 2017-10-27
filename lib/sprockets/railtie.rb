c3ed206d3c75e8757a74189b15c644c7f5a8bb73
David.Gao <david.alpha.fox@gmail.com>
2017-10-28 02:16:35 +0800

    initializer

--- lib/sprockets/railtie.rb
+++ lib/sprockets/railtie.rb
@@ -53,7 +53,9 @@ module Rails
 
   class Engine < Railtie
     # Skip defining append_assets_path on Rails <= 4.2
+    ## if not find append_assets_path in initializers
     unless initializers.find { |init| init.name == :append_assets_path }
+      ## initializer the assets path
       initializer :append_assets_path, :group => :all do |app|
         app.config.assets.paths.unshift(*paths["vendor/assets"].existent_directories)
         app.config.assets.paths.unshift(*paths["lib/assets"].existent_directories)
